7e8f6bc65ce71f5a88fb6e76ba69431f95d14fb5
SHUVAYUdutta12345 <88594920+SHUVAYUdutta12345@users.noreply.github.com>
2021-08-08 18:10:43 +0530

    Initial Commit

---
+++ Hello_World.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Hello_World here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hello_World
+{
+    public static void main(String[] __) {
+        System.out.println("Hello World");
+    }
+}
