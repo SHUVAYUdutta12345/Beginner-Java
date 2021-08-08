67b712f4d7446a0b02929cbcdaa6b399fc5c867d
SHUVAYUdutta12345 <88594920+SHUVAYUdutta12345@users.noreply.github.com>
2021-08-08 18:15:40 +0530

    for loop added

---
+++ Loop.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Loop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Loop
+{
+    public static void main(String[] __){
+        for (int i = 0; i < 5; i++) {
+  System.out.println(i);
+}
+    }
+}
